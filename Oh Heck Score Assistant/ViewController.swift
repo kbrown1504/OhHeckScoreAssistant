18cf9d02ca3ee8da5058e575c082aa1e96f2b00e
Keegan Brown <keegqbrown@gmail.com>
2019-05-17 21:49:53 -0500

    Initial Commit

---
+++ Oh Heck Score Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oh Heck Score Assistant
+//
+//  Created by Keegan Brown on 5/17/19.
+//  Copyright © 2019 Keegan Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
